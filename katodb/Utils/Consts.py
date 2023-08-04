337c03ee1a1f36d6b6cb6de106bd1240728db6b5
konbraphat51 <konbraphat@gmail.com>
2023-08-04 23:58:38 +0900

    アカウント情報取得方法変更

--- katodb/Utils/Consts.py
+++ katodb/Utils/Consts.py
@@ -6,7 +6,7 @@ class Consts:
     
     #要編集（ご自分の環境に合わせて）
     mecab_params = '-u "C:/Program Files/MeCab/dic/NEologd.dic"'
-    nico_pass = "katodb/Scraping/nico_pass.txt"
+    nico_pass = "katodb/Utils/nico_pass.txt"
     
     #多分編集しない
     list_links_file = "Data/list_links.csv"
@@ -14,3 +14,12 @@ class Consts:
     trascription_raw_folder = "Data/Transcription_raw"
     keywords_folder = "Data/Keywords"
     transcription_tokenized_folder = "Data/Transcription_tokenized"
+    
+    def nico_account():
+        '''
+        ニコニコ動画のアカウント情報（メアド、パスワード）を返す
+        '''
+        
+        with open(Consts.nico_pass,  "r") as f:
+            email, password = f.read().split()
+        return (email, password)
