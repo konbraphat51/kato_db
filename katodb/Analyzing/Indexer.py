39959b8a7992aa9521b944275fcce6aad6c7c5e7
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-08-09 16:48:04 +0700

    Dividing index

--- katodb/Analyzing/Indexer.py
+++ katodb/Analyzing/Indexer.py
@@ -13,7 +13,13 @@ class Indexer:
     
     def run(self):
         self.initialize()
-        self.index_all()
+        titles = self.index_all()
+        
+        #分解
+        self.devide_to_subindex(titles)
+        
+        #メインスキーマの削除
+        self.clear_folder(Consts.transcription_index_folder)
     
     def initialize(self):
         '''
@@ -21,21 +27,22 @@ class Indexer:
         '''
         
         #前回のデータを削除する
-        self.clear_folder()
+        self.clear_folder(Consts.transcription_index_folder)
         
         #今回のスキーマ
-        schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True))
-        ix = create_in(Consts.transcription_index_folder, schema)
-        self.writer = ix.writer()
+        self.schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True))
+        self.ix_main = create_in(Consts.transcription_index_folder, self.schema)
         
-    def clear_folder(self):
+    def clear_folder(self, path):
         '''
-        スキーマのあるフォルダーをクリアする
+        フォルダーのファイルのみをクリアする
         '''
-        file_list = os.listdir(Consts.transcription_index_folder)
+        file_list = os.listdir(path)
         
         for filename in file_list:
-            os.remove(Consts.transcription_index_folder + "/" + filename)
+            file_path = path + "/" + filename
+            if os.path.isfile(file_path):
+                os.remove(file_path)
         
     def find_title(self, title):
         '''
@@ -43,7 +50,7 @@ class Indexer:
         ヒット内容のリストを返す。
         '''
         
-        with self.writer.searcher() as searcher:
+        with self.ix_main.searcher() as searcher:
             query = Term("title", title)
             results = searcher.search(query)
             
@@ -58,14 +65,16 @@ class Indexer:
         '''
         
         filenames = self.get_files()
-        for filename in filenames:
-            self.update_index(filename)
-            
-        self.writer.commit()
+        titles = []
+        with self.ix_main.writer() as writer:
+            for filename in filenames:
+                titles.append(self.update_index(filename, writer))
+                
+        return titles
         
-    def update_index(self, filename):
+    def update_index(self, filename, writer):
         '''
-        一つのデータのインデックスを作成する。`self.writer.commit()`すること
+        一つのデータのインデックスを作成する。`writer.commit()`すること
         '''
         
         #情報取得
@@ -76,7 +85,9 @@ class Indexer:
         #     self.writer.delete_by_term("title", title)
             
         #インデックス追加
-        self.writer.add_document(title=title, content=text)
+        writer.add_document(title=title, content=text)
+        
+        return title
     
     def get_files(self):
         '''
@@ -98,13 +109,40 @@ class Indexer:
         '''
     
         #インデックスを取得
-        title = filename.split("/")[-1].split(".")[0]
+        title = self.convert_title(filename.split("/")[-1].split(".")[0])
             
         #セグメントをスペース区切りで結合
         df_segments = pd.read_csv(filename)
         text = "。".join([str(item) for item in df_segments["text"]])
         
         return title, text
+    
+    def devide_to_subindex(self, titles):
+        '''
+        一つあたりのファイルの大きさを抑えるため、サブインデックスを作成
+        '''
+        
+        SINGLE_FILE_N = 300
+        
+        with self.ix_main.searcher() as searcher:
+            for sub_n in range(self.ix_main.doc_count() // SINGLE_FILE_N + 1):
+                #フォルダーの作成
+                folder_name = Consts.transcription_index_folder + "/sub" + str(sub_n)
+                if os.path.exists(folder_name) and os.path.isdir(folder_name):
+                    self.clear_folder(folder_name)
+                else:
+                    os.mkdir(folder_name)
+                    
+                #サブインデックスの作成
+                ix_sub = create_in(folder_name, self.schema)
+                with ix_sub.writer() as writer_sub:
+                    for doc_n in range(SINGLE_FILE_N * sub_n, min(SINGLE_FILE_N * (sub_n + 1), self.ix_main.doc_count())):
+                        document = searcher.search(Term("title", titles[doc_n]))[0]
+                        writer_sub.add_document(title=document["title"], content=document["content"])
+                ix_sub.close()
+    
+    def convert_title(self, title):
+        return title.replace("-", "m")
     
 if __name__ == "__main__":
     indexer = Indexer()
